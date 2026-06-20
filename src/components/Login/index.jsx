5ec8a4a0f8f138004cd1407893ae51d63a8b064b
charanHubCommits <charanmandakuriti@gmail.com>
2026-06-20 20:46:52 +0530

    add initial login page

---
+++ src/components/Login/index.jsx
@@ -0,0 +1,69 @@
+import {useState} from "react"
+import {useNavigate} from "react-router-dom"
+import Cookies from "js-cookie"
+import "./index.css"
+
+const Login = () => {
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+  const [errorMsg,setErrorMsg] = useState("")
+  
+  const navigateHome = useNavigate()
+
+  const signIn = async(event)=>{
+    event.preventDefault()
+
+    const url = "https://v9fes04dwf.execute-api.eu-north-1.amazonaws.com/api/auth/signin" 
+    const options = {
+      method:"POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body:JSON.stringify({ email, password })
+    }
+    const response = await fetch(url,options)
+    const data = await response.json()
+
+    const {token,message} = data
+    if(!token){
+      setErrorMsg(message)
+      console.log(errorMsg)
+    }else {
+      Cookies.set("jwt-token",token,{expires: 7});
+      navigateHome("/")
+    }
+  }
+
+  const onChangeEmail = event=> {
+    setEmail(event.target.value)
+  }
+
+  const onChangePassword = event=> {
+    setPassword(event.target.value)
+  }
+
+  const LoadPage = ()=> {
+    const token = Cookies.get("jwt-token")
+    if(token) return <h1>Go to home page</h1>
+
+    return (  
+      <div className="login-page">
+        <div className="form-container" onSubmit={signIn}>
+        <h1 className="company-name">Go Bussiness</h1>
+        <p className="tag-line">Sign in to open your referral dashboard.</p>
+        <form className="login-form">
+          <label htmlFor="email">Email: </label>
+          <input type="email" id="email" value={email} onChange={onChangeEmail}/>
+          <label htmlFor="password">Password: </label>
+          <input type="password" id="password" value={password} onChange={onChangePassword}/>
+          <button type="submit">Sign in</button>
+        </form>
+        </div>
+      </div>
+    )
+  }
+
+  return LoadPage()
+}
+
+export default Login
