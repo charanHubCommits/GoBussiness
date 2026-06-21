50318b4a3d086dfdb789131f166a94905d71fb3d
charanHubCommits <charanmandakuriti@gmail.com>
2026-06-21 15:34:05 +0530

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import Login from "./components/Login/"
 import Home from './components/Home/'
 import ProtectedRoute from "./components/ProtectedRoute/"
+import ReferralDetails from "./components/ReferralDetails/"
+import NotFound from "./components/NotFound/"
+
 const App = ()=>(
   <BrowserRouter>
     <Routes>
@@ -14,8 +17,22 @@ const App = ()=>(
           </ProtectedRoute>
         }
       />
+      <Route 
+        path="/dashboard/referrals" 
+        element={<Navigate to="/" replace />}
+      />
+      <Route 
+        path="/referral/:id" 
+        element={
+          <ProtectedRoute>
+            <ReferralDetails />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
 )
 
 export default App
+
