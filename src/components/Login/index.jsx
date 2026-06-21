8204d8f79f207ab81141cc762cb1334c4615e650
charanHubCommits <charanmandakuriti@gmail.com>
2026-06-21 15:32:48 +0530

    fix syntax

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,13 +1,13 @@
-import {useState,useEffect} from "react"
-import {useNavigate} from "react-router-dom"
+import { useState, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 import Cookies from "js-cookie"
 import "./index.css"
 
 const Login = () => {
-  const [email,setEmail] = useState("")
-  const [password,setPassword] = useState("")
-  const [errorMsg,setErrorMsg] = useState("")
-  const [showError,setShowError] = useState(false) 
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [errorMsg, setErrorMsg] = useState("")
+  const [showError, setShowError] = useState(false)
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -17,79 +17,78 @@ const Login = () => {
     }
   }, [navigate])
 
-  const signIn = async(event)=>{
+  const signIn = async (event) => {
     event.preventDefault()
 
-    const url = "https://v9fes04dwf.execute-api.eu-north-1.amazonaws.com/api/auth/signin" 
+    const url = "https://v9fes04dwf.execute-api.eu-north-1.amazonaws.com/api/auth/signin"
     const options = {
-      method:"POST",
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body:JSON.stringify({ email, password })
+      body: JSON.stringify({ email, password })
     }
     try {
-      const response = await fetch(url,options)
+      const response = await fetch(url, options)
       const responseJson = await response.json()
       console.log(responseJson)
 
-      if(!response.ok){
-        const {message} = responseJson
+      if (!response.ok) {
+        const { message } = responseJson
         setShowError(true)
         setErrorMsg(message)
         console.log(message)
-      }else {
-        const {data} = responseJson;
-        const {token,message} = data
+      } else {
+        const { data } = responseJson;
+        const { token, message } = data
 
-        Cookies.set("jwt_token",token,{expires: 7});
+        Cookies.set("jwt_token", token, { expires: 7 });
         navigate("/")
       }
     } catch (error) {
-        console.log(error.message)
-        setErrorMsg("Unknown Error")
-        setShowError(true)
+      console.log(error.message)
+      setErrorMsg("Unknown Error")
+      setShowError(true)
     }
   }
 
-  const onChangeEmail = event=> {
+  const onChangeEmail = event => {
     setEmail(event.target.value)
   }
 
-  const onChangePassword = event=> {
+  const onChangePassword = event => {
     setPassword(event.target.value)
   }
 
-  return (  
-      <div className="login-page">
-        <div className="form-container">
-          <h1 className="company-name">Go Business</h1>
-          <p className="tag-line">Sign in to open your referral dashboard.</p>
-          <form className="login-form" onSubmit={signIn}>
-            <label htmlFor="email">Email</label>
-            <input 
-              type="email" 
-              id="email" 
-              value={email} 
-              onChange={onChangeEmail} 
-              placeholder="you@example.com"
-            />
-            
-            <label htmlFor="password">Password</label>
-            <input 
-              type="password" 
-              id="password" 
-              value={password} 
-              onChange={onChangePassword}
-            />
-            
-            {showError && <p className="error-msg">{errorMsg}</p>}
-            <button type="submit">Sign in</button>
-          </form>
-        </div>
+  return (
+    <div className="login-page">
+      <div className="form-container">
+        <h1 className="company-name">Go Business</h1>
+        <p className="tag-line">Sign in to open your referral dashboard.</p>
+        <form className="login-form" onSubmit={signIn}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={onChangeEmail}
+            placeholder="you@example.com"
+          />
+
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={onChangePassword}
+          />
+
+          {showError && <p className="error-msg">{errorMsg}</p>}
+          <button type="submit">Sign in</button>
+        </form>
       </div>
-    )
+    </div>
+  )
 }
 
 export default Login
-
